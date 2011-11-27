a214606641e9e721a540e5dd43743d51606b5fcc
Ali Lown <ali@lown.me.uk>
2011-11-27 19:54:13 +0000

    xc> Remove references to removed files from project

--- xc6/par/pa.fromNetlist.tcl
+++ xc6/par/pa.fromNetlist.tcl
@@ -1,11 +1,11 @@
 
 # PlanAhead Launch Script for Post-Synthesis pin planning, created by Project Navigator
 
-create_project -name scope -dir "/home/ali/Projects/Active/cur/usbscope/fw/xc6slx25/par/planAhead_run_1" -part xc6slx25fgg484-3
+create_project -name scope -dir "/home/ali/Projects/Active/cur/usbscope/fw/xc6/par/planAhead_run_1" -part xc6slx45fgg676-3
 set_property design_mode GateLvl [get_property srcset [current_run -impl]]
-set_property edif_top_file "/home/ali/Projects/Active/cur/usbscope/fw/xc6slx25/par/main.ngc" [ get_property srcset [ current_run ] ]
-add_files -norecurse { {/home/ali/Projects/Active/cur/usbscope/fw/xc6slx25/par} {../ipcore_dir} }
+set_property edif_top_file "/home/ali/Projects/Active/cur/usbscope/fw/xc6/par/main.ngc" [ get_property srcset [ current_run ] ]
+add_files -norecurse { {/home/ali/Projects/Active/cur/usbscope/fw/xc6/par} {../ipcore_dir} }
 set_param project.pinAheadLayout  yes
-set_property target_constrs_file "/home/ali/Projects/Active/cur/usbscope/fw/xc6slx25/scope.ucf" [current_fileset -constrset]
-add_files [list {/home/ali/Projects/Active/cur/usbscope/fw/xc6slx25/scope.ucf}] -fileset [get_property constrset [current_run]]
+set_property target_constrs_file "/home/ali/Projects/Active/cur/usbscope/fw/xc6/scope.ucf" [current_fileset -constrset]
+add_files [list {/home/ali/Projects/Active/cur/usbscope/fw/xc6/scope.ucf}] -fileset [get_property constrset [current_run]]
 open_netlist_design

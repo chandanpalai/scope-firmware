0b9ed41b889dc41fb06285001dd105a8d1bf6c7d
Ali Lown <ali@lown.me.uk>
2012-03-18 20:01:22 +0000

    Add more fx3 impl

--- xc6/src/fx3.vhd
+++ xc6/src/fx3.vhd
@@ -29,9 +29,9 @@ port
   slrd_n  : out std_logic;
   flaga   : in std_logic;
   flgab   : in std_logic;
-  pktend  : out std_logic;
+  pktend_n: out std_logic;
   fifoadr : out std_logic_vector(1 downto 0);
-  dq      : out std_logic_vector(31 downto 0);
+  dq      : inout std_logic_vector(31 downto 0);
 
   --Internal interface
   adcdata    : in std_logic_vector(63 downto 0);
@@ -50,7 +50,96 @@ end fx3;
 architecture Behavioral of fx3 is
 ---------------------------------------------------------------------------
 
+  constant WR_ADC : std_logic := '0';
+  constant WR_CFG : std_logic := '1';
+
+  constant OUTEPCFG: std_logic_vector(1 downto 0) := "00"; --EP2
+  constant INEPCFG : std_logic_vector(1 downto 0) := "01"; --EP4
+  constant INEPADC : std_logic_vector(1 downto 0) := "01"; --EP6
+
+  --FSM
+  type state_type is (st0_default,
+    st1_r_assertfifo, st2_r_sloe, st3_r_slame, st4_r_deassert, st5_r_next,
+    st1_w_assertfifo, st2_w_data, st3_w_pulse, st4_w_next);
+  signal state, next_state : state_type;
+  --Keep track for clearing a packet when left idle
+  signal byte_count     : unsigned(9 downto 0);
+  signal inactive_count : unsigned(5 downto 0);
+
+  signal writewhich : std_logic := WR_ADC;
+
 begin
+  fsm : process(clk, sys_rst)
+  begin
+    if clk'event and clk = '1' then
+      if reset = '1' then
+        state <= st0_default;
+        inactive_count <= TO_UNSIGNED(0, 6);
+        byte_count <= TO_UNSIGNED(0, 10);
+      else
+        case state is
+          when st0_default =>
+            slcs_n  <= '1';
+            slwr_n  <= '1';
+            sloe_n  <= '1';
+            slrd_n  <= '1';
+            fifoadr <= "00";
+            dq      <= (others => 'Z');
+            if inactive_count = 63 and (not (byte_count = 0)) then
+              pktend_n <= '0';
+              inactive_count <= TO_UNSIGNED(0, 6);
+              byte_count <= TO_UNSIGNED(0, 10);
+            else
+              pktend_n <= '1';
+              inactive_count <= inactive_count + 1;
+            end if;
+
+            --read states
+          when st1_r_assertfifo =>
+            slcs_n <= '0';
+            fifoadr <= OUTEPCFG;
+          when st2_r_sloe =>
+            sloe_n <= '0';
+          when st3_r_sample =>
+            slrd_n <= '0';
+            cfgout <= dq;
+            cfgoutclk <= '1';
+          when st4_r_deassert =>
+            slrd_n <= '1';
+          when st5_r_next =>
+            cfgoutclk <= '0';
+
+            --write states
+          when st1_w_assertfifo =>
+            inactive_count <= TO_UNSIGNED(0, 6);
+            slcs_n         <= '0';
+            if writewhich = WR_ADC then
+              fifoadr <= INEPADC;
+            else
+              fifoadr <= INEPCFG;
+            end if;
+          when st2_w_data =>
+            pktend_n <= '1';
+            slwr_n   <= '0';
+            if writewhich = WR_ADC then
+              --dq <= adc_dout;
+            else
+              --dq <= cfg_dout;
+            end if;
+            byte_count <= byte_count + 1;
+          when st3_w_pulse =>
+            slwr_n <= '1';
+          when st2_w_next =>
+            if flaga = '0' then
+              dq <= (others => 'Z');
+              pktend_n <= '0';
+              byte_count <= TO_UNSIGNED(0, 10);
+              inactive_count <= TO_UNSIGNED(0, 6);
+            end if;
+        end case;
+      end if;
+    end if;
+  end process;
 
 end architecture Behavioral;
 
